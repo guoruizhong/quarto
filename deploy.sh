3bada2cd8fb513db0881db416fcd3ce9f5f23fc3
guoruizhong <grgrzhong@gmail.com>
2023-03-06 21:56:28 +0800

    R colors and barplot with same width

--- deploy.sh
+++ deploy.sh
@@ -14,5 +14,5 @@ quarto render
 # Add to the local git repository then push to the remote repository
 
 git add .
-git commit -m "update pipeline"
+git commit -m "R colors and barplot with same width"
 git push -u guoruizhong.github.io main
