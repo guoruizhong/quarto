926e743fb1ac8b463cfdda6488ebc6a5780f938d
guoruizhong <grgrzhong@gmail.com>
2023-09-09 23:45:37 +0800

    scRNAseq

--- update.sh
+++ update.sh
@@ -14,5 +14,5 @@ quarto render
 # Add to the local git repository then push to the remote repository
 
 git add .
-git commit -m "use lower case"
+git commit -m "scRNAseq"
 git push -u guoruizhong.github.io main
